43557fefb4bd362b85400ab41383cc39bb4da270
SergeyMot <MotailenkoSU@tomskasu.ru>
2022-03-09 10:10:21 +0700

    fix copyright and delete page About

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,14 @@ const Footer = () => {
   return (
     <footer className="page-footer font-small bg-dark mt-5">
       <div className="footer-copyright text-center py-3 text-white">
-        © {"2020 - " + year} Sergey Motaylenko
+        © {"2020 - " + year}{" "}
+        <a
+          href="https://sergey-motaylenko.ru/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Sergey Motaylenko
+        </a>
       </div>
     </footer>
   );
